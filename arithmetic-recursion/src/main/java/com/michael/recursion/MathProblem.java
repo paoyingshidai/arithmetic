86448c3c9b01acfe675f0e49bd4e5f723cdc5bfb
Michael <13432849094@163.com>
2018-11-26 18:13:45 +0800

    commit

---
+++ arithmetic-recursion/src/main/java/com/michael/recursion/MathProblem.java
@@ -0,0 +1,24 @@
+package com.michael.recursion;
+
+public class MathProblem {
+
+
+    public void fabonaqi(String a, String b, int num) {
+
+        if (num == 1) {
+            System.out.println(a + " ----> " + b);
+        } else {
+
+        }
+
+
+
+    }
+
+
+
+    public static void main(String[] args) {
+
+    }
+
+}

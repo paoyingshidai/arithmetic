dbe550e280fbfed54174a2f7da1504bdf41df3a7
Michael <13432849094@163.com>
2019-09-04 14:32:23 +0800

    commit

--- arithmetic-sort/src/main/java/com/michael/sort/InsertSort.java
+++ arithmetic-sort/src/main/java/com/michael/sort/InsertSort.java
@@ -19,28 +19,10 @@ public class InsertSort {
 	    return arr;
 	}
 
-
-	public static Integer[] mySort(Integer[] arr) {
-
-		for (int i = 1; i < arr.length; i++) {
-			int preIndex = i - 1;
-			Integer selected = arr[i];
-
-			while (preIndex >= 0 && arr[preIndex] > selected) {
-				arr[preIndex + 1] = arr[preIndex];
-				preIndex--;
-			}
-			arr[preIndex+1] = selected;
-		}
-		return arr;
-	}
-
-
-
 	public static void main(String[] args) {
 		Integer[] arr = {2, 5, 6, 9, 8, 1, 3};
-//		insertionSort(arr);
-		mySort(arr);
+		insertionSort(arr);
+//		mySort(arr);
 		for (int i = 0; i < arr.length; i++) {
 			System.out.println(arr[i]);
 		}

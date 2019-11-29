f502d8068b2724c71e70dbb01ef104f4eb949beb
Michael <13432849094@163.com>
2019-11-29 10:36:49 +0800

    commit

--- arithmetic-sort/src/main/java/com/michael/sort/InsertSort.java
+++ arithmetic-sort/src/main/java/com/michael/sort/InsertSort.java
@@ -10,11 +10,11 @@ public class InsertSort {
 	    for (int i = 1; i < len; i++) {
 	        preIndex = i - 1;
 	        current = arr[i];
-	        while(preIndex >= 0 && arr[preIndex] > current) {
+	        while(preIndex >= 0 && arr[preIndex] > current) {	// 向后移动
 	            arr[preIndex+1] = arr[preIndex];
 	            preIndex--;
 	        }
-	        arr[preIndex+1] = current;
+	        arr[preIndex+1] = current;							// 插入元素
 	    }
 	    return arr;
 	}

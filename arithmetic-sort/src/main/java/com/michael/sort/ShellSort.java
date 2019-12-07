5d242b433363cc9e8162596cff5dbe612a8632b8
Michael <13432849094@163.com>
2019-12-07 15:48:06 +0800

    希尔排序，使用插入排序的方法重写

--- arithmetic-sort/src/main/java/com/michael/sort/ShellSort.java
+++ arithmetic-sort/src/main/java/com/michael/sort/ShellSort.java
@@ -6,9 +6,10 @@ public class ShellSort {
 
     /**
      * just like insertSort plus;
+     *
      * @param data
      */
-	public static void shellSortSmallToBig(int[] data) {
+    public static void shellSortSmallToBig(int[] data) {
         int j = 0;
         int temp = 0;
         for (int increment = data.length / 2; increment > 0; increment /= 2) {
@@ -31,30 +32,28 @@ public class ShellSort {
 
 
     /**
-     *
      * @param data
      */
     public static void mySort(int[] data) {
         for (int increment = data.length / 2; increment > 0; increment /= 2) {
-             for (int i = increment; i < data.length; i++) {
-                 int temp = data[i];
-                 int j;
-                 for (j = i - increment; j >= 0; j -= increment) {
-                     if (temp < data[j]) {      // 这里的 大于或者小于 就决定了升序或者降序
-                         data[j + increment] = data[j]; // 将大的数放在后面
-                     } else {
-                         break;
-                     }
-                 }
-                 data[j + increment] = temp;
-             }
+            for (int i = increment; i < data.length; i++) {
+                int temp = data[i];
+                int j = i - increment;
+
+                // 这是插入排序的写法
+                while (j >= 0 && temp < data[j]) {
+                    data[j + increment] = data[j];
+                    j -= increment;
+                }
+                data[j + increment] = temp;
+            }
         }
     }
 
 
     public static void main(String[] args) {
 //        int[] data = new int[] { 26, 53, 67, 48, 57, 13, 48, 32, 60, 50 };
-        int[] data = new int[] {60, 50, 26, 53, 67, 48, 57, 13, 48, 32, 1};
+        int[] data = new int[]{60, 50, 26, 53, 67, 48, 57, 13, 48, 32, 1};
         System.out.println(Arrays.toString(data));
 //        shellSortSmallToBig(data);
         mySort(data);
